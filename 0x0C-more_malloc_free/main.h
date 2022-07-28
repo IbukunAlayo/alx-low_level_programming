48de7349fc37f2dcf7e7ada9a15b0abfa24c6698
Sarah Alayo <sarahalayo0@gmail.com>
2022-07-28 13:08:43 -0700

    Task 0

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,16 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+#include <stdlib.h>
+#include <stdio.h>
+
 int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
