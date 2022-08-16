c634eb7a315399e92d7a307c99dffc1396c5ab4a
Sarah Alayo <sarahalayo0@gmail.com>
2022-08-16 15:25:44 -0700

    Task 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -4,11 +4,9 @@
 #include "lists.h"
 
 /**
-* insert_nodeint_at_index - a function that inserts a new
-*node at a given position
+* insert_nodeint_at_index - function that inserts new node at a given position
 * @head: a pointer to a pointer to a struct
-* @idx: is the index of the list where the new node
-*should be added. Index starts at 0
+* @idx: is the index of the list where the new node should be added.
 * @n: the data for the new node
 * Return: the address of the new node, or NULL if it failed
 */
